ca268209696e16878ac7fc3f61ec58f2ccfd6e66
iagpomponet <iagosilbernagel@hotmail.com>
2021-02-22 22:39:05 -0300

    iterative factorial

--- index.scm
+++ index.scm
@@ -1,2 +1,13 @@
+// recursive version
 (define fact (lambda (x)(
   if(= n 1) 1 (* n (fact (- n 1))))))
+
+// iterative version
+(define fact (lambda (n) (fact-helper 1 1 n)))
+(define fact-helper (lambda (product counter n)(
+  if(> counter n)
+    product
+    (fact-helper(* product counter) (+ counter 1) n)
+)))
+(fact 5)
+
